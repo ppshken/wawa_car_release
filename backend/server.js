a93e07401b7c57ba2850dd33c9a1df32ce87fdd3
ppshken <kensaohin@gmail.com>
2026-08-05 21:09:53 +0700

    lasted

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,13 @@ const app = express();
 const PORT = process.env.PORT;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://187.127.216.219'
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(auditRequestLogger);
